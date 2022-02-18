2fc73ddf332a099ffb73c30b14f16c81ac6bb331
Igor R. Shakhbazyan <shahidsecret@gmail.com>
2022-02-18 17:33:31 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ templates_path = ['_templates']
 
 html_theme = 'classic',
 html_theme_options = {
-    "bgcolor": "#8a2be2",
-    "textcolor": "black"
+    "bgcolor": "8a2be2",
+    "textcolor": "black",
 }
 
 # -- Options for EPUB output
