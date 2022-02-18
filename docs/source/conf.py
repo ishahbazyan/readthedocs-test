5164d82fda7d7173f6f842cb269309dafba48f27
Igor R. Shakhbazyan <shahidsecret@gmail.com>
2022-02-18 22:29:54 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = "sphinx-rtd-theme",
+html_theme = "sphinx-rtd-theme"
 # html_theme = 'css/custom.css'
 # html_theme_options = {
 #    "background-color": "8a2be2",
